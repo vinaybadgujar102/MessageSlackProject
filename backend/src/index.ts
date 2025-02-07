562704a349d263acbc22d872a528e3a650aa7ae4
Vinay Badgujar <vinaybadgujar8@gmail.com>
2025-02-07 22:27:40 +0530

    added user schema with robohash profile

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,19 +1,21 @@
 import express from 'express'
 import { StatusCodes } from 'http-status-codes'
 
-import { PORT } from './config/serverConfig'
 import { dbConfig } from './config/dbConfig'
+import { PORT } from './config/serverConfig'
+import apiRoutes from './routes/apiRoutes'
 const app = express()
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
+app.use('/api', apiRoutes)
 app.get('/ping', (req, res) => {
   res.status(StatusCodes.OK).json({ message: 'pong' })
 })
 
 app.listen(PORT, async () => {
-  await dbConfig.connect().then(() => {
-    console.log(`Server is running on port ${PORT}`)
-  })
+  //   await dbConfig.connect().then(() => {
+  //     console.log(`Server is running on port ${PORT}`)
+  //   })
 })
---
+++ backend/src/schema/user.ts
@@ -0,0 +1,38 @@
+import { model, Schema } from 'mongoose'
+
+const userSchema = new Schema(
+  {
+    email: {
+      type: String,
+      unique: [true, 'Email already exists'],
+      required: [true, 'Email is required'],
+      match: [
+        /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+        'Please use a valid email address'
+      ]
+    },
+    password: {
+      type: String,
+      required: [true, 'Password is required'],
+      minlength: [8, 'Password must be at least 8 characters long']
+    },
+    username: {
+      type: String,
+      required: [true, 'Username is required'],
+      minlength: [3, 'Username must be at least 3 characters long']
+    },
+    avatar: {
+      type: String
+    }
+  },
+  { timestamps: true }
+)
+
+userSchema.pre('save', async function (next) {
+  this.avatar = `https://robohash.org/${this.username}.png`
+  next()
+})
+
+const User = model('User', userSchema)
+
+export default User
